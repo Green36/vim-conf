008603a3ffa9476a07e5aea0cd195c3fc5525c73
kkk <k@k>
2014-11-08 06:04:52 +0900

    update

--- linux_setting.sh
+++ linux_setting.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-cd $(dirname $0)
-path=`echo $(pwd)`
+UPDATE_FILES=( \
+    "vimfiles,.vim" \
+    "vimrc,.vimrc" \
+    "gvimrc,.gvimrc" \
+    )
 
-rm -Rf ~/.vim
-ln -s ${path}/vimfiles ~/.vim 
-cp ${path}/vimrc ~/.vimrc
-cp ${path}/gvimrc ~/.gvimrc
+CUR_DIR=`cd $(dirname $0) && pwd`
+BACKUP_DIR="VimSettingBackup_"$( date "+%Y%m%d%H%M%S" )
+
+mkdir ~/${BACKUP_DIR}
+for file in ${UPDATE_FILES[@]}
+do
+    file=( `echo $file | tr ',' ' '` )
+    if [ -e ~/${file[1]} ]; then
+        mv ~/${file[1]} ~/${BACKUP_DIR}/
+    fi
+    ln -s ${CUR_DIR}/${file[0]} ~/${file[1]}
+done
 
 if [ ! -e ~/.vim/NeoBundle/neobundle.vim ]; then
     git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/NeoBundle/neobundle.vim/
 fi
-
 
