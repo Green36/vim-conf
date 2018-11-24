024696d5276627c94dbdc616ebb7bb221b80713e
y <y@y>
2018-11-24 15:43:07 +0900

    modify for neovim.

--- linux_setting.sh
+++ linux_setting.sh
@@ -3,8 +3,14 @@
 UPDATE_FILES=( \
     "vimfiles,.vim" \
     "vimrc,.vimrc" \
+    "vimrc,.config/nvim/init.vim" \
     "gvimrc,.gvimrc" \
     )
+
+if [ ! -e ~/.config/nvim ]; then
+    mkdir -p ~/.config/nvim
+    echo "created ~/.config/nvim"
+fi
 
 CUR_DIR=`cd $(dirname $0) && pwd`
 BACKUP_DIR="VimSettingBackup_"$( date "+%Y%m%d%H%M%S" )
